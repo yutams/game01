f1c583bf63d6f9d5ea50992a967c960b189ee2a0
Yuta Mizuno <yuta.mizuno.j2a@jp.denso.com>
2025-01-19 18:09:26 +0900

    update

---
+++ make_html.bat
@@ -0,0 +1,46 @@
+@echo off
+:: エラー発生時に処理を停止
+setlocal enabledelayedexpansion
+set ERRORLEVEL=0
+
+:: 引数チェック
+if "%~1"=="" (
+    echo エラー: dir_name を指定してください。
+    exit /b 1
+)
+if "%~2"=="" (
+    echo エラー: file_name を指定してください。
+    exit /b 1
+)
+
+:: 引数を変数に代入
+set "dir_name=%~1"
+set "file_name=%~2"
+
+:: パッケージコマンドの実行
+pyxel package "%dir_name%" "%dir_name%\%file_name%.py"
+if not !ERRORLEVEL!==0 (
+    echo エラー: pyxel package コマンドが失敗しました。
+    exit /b 1
+)
+
+:: app2html コマンドの実行
+pyxel app2html "%dir_name%.pyxapp"
+if not !ERRORLEVEL!==0 (
+    echo エラー: pyxel app2html コマンドが失敗しました。
+    exit /b 1
+)
+
+:: ファイルのリネーム
+if exist "index.html" (
+    del "index.html"
+)
+rename "%dir_name%.html" "index.html"
+if not !ERRORLEVEL!==0 (
+    echo エラー: rename に失敗しました。
+    exit /b 1
+)
+
+:: 完了メッセージ
+echo 処理が完了しました。
+exit /b 0
--- test/09_shooter.py
+++ test/09_shooter.py
@@ -189,6 +189,15 @@ class Blast:
 class App:
     def __init__(self):
         pyxel.init(120, 160, title="Pyxel Shooter")
+
+        # 仮想コントローラーの位置を設定
+        pyxel.controller(
+            True,  # 仮想コントローラーを表示
+            x=20,  # コントローラー左上のx座標
+            y=80,  # コントローラー左上のy座標
+            scale=1.5,
+        )  # コントローラーの拡大率
+
         pyxel.images[0].set(
             0,
             0,
